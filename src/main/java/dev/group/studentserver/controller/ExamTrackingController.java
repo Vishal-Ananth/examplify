e6f127fd2f3237d6e0b8fc7851546da9e49fd211
Vishal Ananth J <imvishal2002@gmail.com>
2023-11-03 23:28:03 +0530

    feat: linking student exam details with answer script identity

--- src/main/java/dev/group/studentserver/controller/ExamTrackingController.java
+++ src/main/java/dev/group/studentserver/controller/ExamTrackingController.java
@@ -1,25 +1,25 @@
-//package dev.group.studentserver.controller;
-//
-//import dev.group.studentserver.service.ExamTrackingService;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("api/examtracking")
-//public class ExamTrackingController {
-//
-//    private final ExamTrackingService examTrackingService;
-//
-//    public ExamTrackingController(ExamTrackingService examTrackingService) {
-//        this.examTrackingService = examTrackingService;
-//    }
-//
-//    @GetMapping
-//    public String testing(){
-//        return "Hello World";
-//    }
-//
-//    @PostMapping("link")
-//    public String createTrackingTicket(@RequestParam Integer rollNumber,@RequestParam String subCode,@RequestParam String answerScriptId){
-//        return examTrackingService.createTrackingTicket(rollNumber,answerScriptId);
-//    }
-//}
+package dev.group.studentserver.controller;
+
+import dev.group.studentserver.service.ExamTrackingService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/examtracking")
+public class ExamTrackingController {
+
+    private final ExamTrackingService examTrackingService;
+
+    public ExamTrackingController(ExamTrackingService examTrackingService) {
+        this.examTrackingService = examTrackingService;
+    }
+
+    @GetMapping
+    public String testing(){
+        return "Hello World";
+    }
+
+    @PostMapping("link")
+    public String createTrackingTicket(@RequestParam Integer rollNumber,@RequestParam String subCode,@RequestParam String answerScriptId){
+        return examTrackingService.createTrackingTicket(rollNumber,subCode,answerScriptId);
+    }
+}
--- src/main/java/dev/group/studentserver/service/ExamTrackingService.java
+++ src/main/java/dev/group/studentserver/service/ExamTrackingService.java
@@ -1,19 +1,33 @@
-//package dev.group.studentserver.service;
-//
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class ExamTrackingService {
-//
-//    private final StudentService studentService;
-//
-//    public ExamTrackingService(StudentService studentService) {
-//        this.studentService = studentService;
-//    }
-//
-//    public String createTrackingTicket(Integer rollNumber, String answerScriptId) {
-//        return "createTrackingTicket";
-//    }
-//
-//
-//}
+package dev.group.studentserver.service;
+
+import dev.group.studentserver.dao.ExamTrackingRepository;
+import org.springframework.stereotype.Service;
+
+import dev.group.studentserver.model.*;
+
+@Service
+public class ExamTrackingService {
+
+    private final StudentService studentService;
+    private final SubjectService subjectService;
+
+    private final ExamTrackingRepository examTrackingRepository;
+
+    public ExamTrackingService(StudentService studentService, SubjectService subjectService, ExamTrackingRepository examTrackingRepository) {
+        this.studentService = studentService;
+        this.subjectService = subjectService;
+        this.examTrackingRepository = examTrackingRepository;
+    }
+
+
+    public String createTrackingTicket(Integer rollNumber, String subCode, String answerScriptId) {
+        Student examinee = studentService.findStudentByRollNumber(rollNumber);
+        Subject course = subjectService.getSubjectByCode(subCode);
+        ExamTracking examTracking = new ExamTracking(examinee,course,answerScriptId);
+
+        examTrackingRepository.save(examTracking);
+
+        return "Register Number "+examinee.getRollNumber()+" writing "+course.getSubCode()+" has been linked to answer script";
+
+    }
+}
